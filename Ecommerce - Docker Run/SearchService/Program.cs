661db0b2a114be9f912f104a73ce06a03b4b3565
Dev Thakkar <thakkdev@gmail.comn>
2023-10-09 18:19:45 -0400

    Docker Run and Kubernetes run

---
+++ Ecommerce - Docker Run/SearchService/Program.cs
@@ -0,0 +1,49 @@
+using ECommerce.Api.Search.Interfaces;
+using ECommerce.Api.Search.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddScoped<IProductsService, ProductsService>();
+builder.Services.AddScoped<IOrdersService, OrdersService>();
+builder.Services.AddScoped<ICustomersService, CustomersService>();
+builder.Services.AddScoped<ISearchService, ECommerce.Api.Search.Services.SearchService>();
+
+builder.Services.AddHttpClient("ProductsService", config =>
+{
+    //config.BaseAddress = new Uri(Configuration["Services:Products"]);
+    config.BaseAddress = new Uri(builder.Configuration.GetValue<string>("Services:Products"));
+
+});
+builder.Services.AddHttpClient("OrdersService", config =>
+{
+    //config.BaseAddress = new Uri(Configuration["Services:Orders"]);
+    config.BaseAddress = new Uri(builder.Configuration.GetValue<string>("Services:Orders"));
+});
+builder.Services.AddHttpClient("CustomersService", config =>
+{
+    // config.BaseAddress = new Uri(Configuration["Services:Customers"]);
+    config.BaseAddress = new Uri(builder.Configuration.GetValue<string>("Services:Customers"));
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Ecommerce - Kubernetes Run/OrderService/Program.cs
@@ -0,0 +1,41 @@
+using ECommerce.Api.Orders.Db;
+using ECommerce.Api.Orders.Interfaces;
+using ECommerce.Api.Orders.Providers;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<OrdersDbContext>(options =>
+{
+    options.UseInMemoryDatabase("Orders");
+});
+builder.Services.AddScoped<IOrdersProvider, OrdersProvider>();
+builder.Services.AddAutoMapper(typeof(Program));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
